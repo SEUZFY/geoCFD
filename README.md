fbbbfa8c16fd114b8f787ff0c2ec01993a72cc18
SEUZFY <zmocheng@gmail.com>
2022-09-27 16:41:29 +0200

    添加项目文件。

---
+++ README.md
@@ -0,0 +1,57 @@
+# geoCFD
+
+It's a x64-windows10 version of [geocfd](https://github.com/SEUZFY/geocfd). Further development may be performed in this project since a discovery has been found 
+
+that keeping using `WSL-Ubuntu` will occupy more and more space in C drive.
+
+Purpose: Process geometry for cfd simulation.
+
+`Now`:
+
+- Read two adjacent buildings, process repeated vertices and build two `nef polyhedra`.
+
+- Union two `nef polyhedra` into one `big nef polyhedron`.
+
+- Export the `big nef polyhedron` as `.cityjson` file(with no repeated vertices) and visualise it in [ninja](https://ninja.cityjson.org/), observe its `exterior` and `interior`
+
+- Get the `convex hull` of the `big nef polyhedron` and visualise it in [ninja](https://ninja.cityjson.org/), observe its `exterior` and `interior`
+
+`To do`:
+
+- test more complicated buildings 
+
+  - how much will the shape change?
+  
+  - [3D Minkowski Sum](https://doc.cgal.org/latest/Minkowski_sum_3/index.html#Chapter_3D_Minkowski_Sum_of_Polyhedra) - can we do sum in some specific direction?
+ 
+ `long term`
+ 
+  - `robust`
+  
+  - `val3dity`  - [validate](http://geovalidation.bk.tudelft.nl/val3dity/) the geometry
+  
+  - `validator` - [validate](https://validator.cityjson.org/) the `cityjson` file
+  
+  - `#include` - include multiple files, how to avoid possibly messy includings?
+
+## Prerequisite
+
+[CGAL](https://www.cgal.org/) - The version should be above `5.0` since we can use the `header-only`, which means we don't have to manually compile `CGAL`.
+
+install `CGAL` via `vcpkg`:
+
+check this -> [vcpkg Crash Course | C++ libraries simplified! A Visual Studio must-have!](https://www.youtube.com/watch?v=b7SdgK7Y510)
+
+## How to use?
+
+The files are built and executed on `x64-windows10` platform.
+
+Clone this project and open it in `Visual Studio 2019` or newer version, you should be able to directly build and run.
+
+## Compile & run
+
+Compiler: `cl.exe`
+
+## Other platforms
+
+If you use other platforms (such as `Linux` or `MacOS`), you can refer to `CMakeLists.txt` file and use it to build a `CMake` project using `src`, `include` and `data` folder.
---
+++ src/JsonHandler.hpp
@@ -0,0 +1,199 @@
+﻿#pragma once
+
+// include files
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "json.hpp"
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+
+// typedefs
+typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
+typedef Kernel::Point_3 Point_3;
+
+// using declaration
+using json = nlohmann::json;
+
+// define constant epsilon - tolerance
+const double epsilon = 1e-8;
+
+// struct definitions - for reading from cityjson and store the information
+struct Ring
+{
+	std::vector<unsigned long> indices;
+
+	// ... may add other attributes
+	// indicating the indices in "vertices"
+	// e.g.
+	// surface[[0,1,2,3], [4,5,6,7]] is a surface which has two rings:
+	// [0,1,2,3] and [4,5,6,7]
+};
+
+struct Face
+{
+	std::vector<Ring> rings;
+
+	// ... may add other attributes
+	// e.g. 
+	// [[0,1,2,3]] is a surface without any holes
+	// [[0,1,2,3], [4,5,6,7]] is a surface(containing a hole) - has two rings
+	// this surface has two rings: [0,1,2,3], [4,5,6,7]
+	// for most surfaces, it only has one ring - e.g. surface [[1,2,6,5]] only has one ring: [1,2,6,5]
+};
+
+struct Shell
+{
+	std::vector<Face> faces;
+
+	// ... may add other attributes
+	// e.g.
+	// [[[0,1,2,3]], [[3,4,5,6]], [[4,5,6,7]], [[7,8,9,10]]] - a shell containing 4 surfaces
+};
+
+struct Solid
+{
+	std::vector<Shell> shells; // store the shells
+	std::string id; // store the building id, if needed
+	double lod; // store the lod level, if needed - lod must be converted to string(i.e. "1.3") when writing to file
+
+	// ... may add other attributes
+	// principally one solid can contain multiple shells
+	// generally in cityjson file one solid contains one shell
+	// e.g.
+	// [
+	// [[[0,1,2,3]], [[3,4,5,6]], [[4,5,6,7]], [[7,8,9,10]]], -- shell 1
+	// [[[0,2,1,3]], [[6,5,4,3]], [[7,6,5,4]], [[10,9,8,7]]], -- shell 2
+	// ]
+};
+// end of struct definitions - for reading from cityjson and store the information
+
+
+
+// handle cityjson file
+class JsonHandler
+{
+protected:
+	/*
+	* check if a vertex already exists in a vertices vector
+	* use coordinates to compare whether two vertices are the same
+	* return: False - not exist, True - already exist
+	*/
+	bool vertex_exist_check(std::vector<Point_3>& vertices, const Point_3& vertex) {
+		bool flag(false);
+		for (const auto& v : vertices) {
+			if (
+				abs(CGAL::to_double(vertex.x()) - CGAL::to_double(v.x())) < epsilon &&
+				abs(CGAL::to_double(vertex.y()) - CGAL::to_double(v.y())) < epsilon &&
+				abs(CGAL::to_double(vertex.z()) - CGAL::to_double(v.z())) < epsilon) {
+				flag = true;
+			}
+		}
+		return flag;
+	}
+
+
+	/*
+	* if a vertex is repeated, find the index in vertices vector and return the index
+	*/
+	unsigned long find_vertex_index(std::vector<Point_3>& vertices, Point_3& vertex) {
+		for (std::size_t i = 0; i != vertices.size(); ++i) {
+			if (
+				abs(CGAL::to_double(vertex.x()) - CGAL::to_double(vertices[i].x())) < epsilon &&
+				abs(CGAL::to_double(vertex.y()) - CGAL::to_double(vertices[i].y())) < epsilon &&
+				abs(CGAL::to_double(vertex.z()) - CGAL::to_double(vertices[i].z())) < epsilon) {
+				return (unsigned long)i;
+			}
+		}
+		std::cout << "warning: please check find_vertex_index function, no index found" << '\n';
+		return 0;
+	}
+
+public:
+	/*
+	* CityJSON files have their vertices compressed : https://www.cityjson.org/specs/1.1.1/#transform-object
+	* this function visits all the surfaces of a certain building
+	* and print the (x,y,z) coordinates of each vertex encountered
+	* lod specified: 1.3
+	*/
+	void read_certain_building(const json& j, const std::string& building_id) {
+		for (auto& co : j["CityObjects"].items()) {
+			if (co.key() == building_id)
+			{
+				std::cout << "CityObject: " << co.key() << std::endl;
+				for (auto& g : co.value()["geometry"]) {
+					if (g["type"] == "Solid" && (abs(g["lod"].get<double>() - 1.3)) < epsilon) { // geometry type: Solid, use lod1.3
+						std::cout << "current lod level: " << g["lod"].get<double>() << '\n';
+						Solid so; // create a solid to store the information
+						so.id = co.key(); // store id
+						so.lod = g["lod"].get<double>(); // store lod info
+						for (auto& shell : g["boundaries"]) {
+							Shell se;
+							for (auto& surface : shell) {
+								Face f; // create a face
+								for (auto& ring : surface) {
+									Ring r; // create a ring
+									//std::cout << "---" << std::endl;
+									for (auto& v : ring)
+									{
+										std::vector<int> vi = j["vertices"][v.get<int>()];
+										double x = (vi[0] * j["transform"]["scale"][0].get<double>()) + j["transform"]["translate"][0].get<double>();
+										double y = (vi[1] * j["transform"]["scale"][1].get<double>()) + j["transform"]["translate"][1].get<double>();
+										double z = (vi[2] * j["transform"]["scale"][2].get<double>()) + j["transform"]["translate"][2].get<double>();
+
+										// when adding new vertex and adding new index in r.indices, check repeatness
+										Point_3 new_vertex(x, y, z);
+										bool if_existed = vertex_exist_check(vertices, new_vertex);
+										if (!if_existed) {
+											vertices.emplace_back();
+											vertices.back() = new_vertex; // if not existed yet, add it to vertices vector
+											r.indices.emplace_back((unsigned long)vertices.size() - 1); // add new index to this ring's indices vector
+											// since we add new vertex to vertices, vertices.size()-1 represents the last index
+											// can also do: r.indices.emplace_back(index++);
+										}
+										else {
+											unsigned long exist_index = find_vertex_index(vertices, new_vertex);
+											r.indices.emplace_back();
+											r.indices.back() = exist_index; // if existed, add the exist index to this ring's indices vector
+										}
+
+										// vertices.emplace_back();
+										// vertices.back() = Point_3(x, y, z); // add the vertex to the vertices vector
+										// std::cout << v << " (" << x << ", " << y << ", " << z << ")" << '\n';
+										// r.indices.emplace_back((unsigned long)vertices.size()-1); // new indices 0-based
+
+									} // end for: each indice in one ring
+									f.rings.emplace_back(r); // add ring to the surface
+								}// end for: each ring in one surface
+								se.faces.emplace_back(f);
+							} // end for: each surface in one shell
+							so.shells.emplace_back(se);
+						}// end for: each shell in one solid
+						solids.emplace_back(so);
+					}// end if: solid and lod = 1.3
+				}
+			}
+		}
+	}
+
+
+
+	/*
+	* prompt basic information of the current building
+	*/
+	void message()
+	{
+		std::cout << "---------building(part) info----------\n";
+		std::cout << "building(part) name: " << solids[0].id << '\n';
+		std::cout << "lod level: " << solids[0].lod << '\n';
+		std::cout << "number of vertices: " << vertices.size() << '\n';
+		std::cout << "--------------------------------------\n";
+	}
+
+
+public:
+	std::vector<Point_3> vertices; // store all vertices of one building
+	std::vector<Solid> solids; // store all solids of one building, ideally one solid for each building
+};
+
